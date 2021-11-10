28d8a562fc51f0f653e637a9672f226e7002b725
JayawardanaJTH <tiran2323@gmail.com>
2021-11-11 00:07:34 +0530

    Create student model

---
+++ Student_Enrolment_System/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Enrolment_System.Models
+{
+    class Student
+    {
+        private int regno { get; set; }
+        private string name { get; set; }
+        private DateTime dob { get; set; }
+        private char gender { get; set; }
+        private int contact { get; set; }
+        private string course { get; set; }
+
+        public Student(int regno, string name, DateTime dob, char gender, int contact, string course)
+        {
+            this.regno = regno;
+            this.name = name;
+            this.dob = dob;
+            this.gender = gender;
+            this.contact = contact;
+            this.course = course;
+        }
+    }
+}
