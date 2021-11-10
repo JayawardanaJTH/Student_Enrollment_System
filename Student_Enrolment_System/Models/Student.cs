e949cee406bf7cec3ae37ea64a83cbb9f2ac8c82
JayawardanaJTH <tiran2323@gmail.com>
2021-11-11 00:32:49 +0530

    add geters and setters

--- Student_Enrolment_System/Models/Student.cs
+++ Student_Enrolment_System/Models/Student.cs
@@ -8,21 +8,57 @@ namespace Student_Enrolment_System.Models
 {
     class Student
     {
-        private int regno { get; set; }
-        private string name { get; set; }
-        private DateTime dob { get; set; }
-        private char gender { get; set; }
-        private int contact { get; set; }
-        private string course { get; set; }
+        private int _regno;
+        private string _name;
+        private DateTime _dob;
+        private char _gender;
+        private int _contact;
+        private string _course;
 
-        public Student(int regno, string name, DateTime dob, char gender, int contact, string course)
+            public Student(int regno, string name, DateTime dob, char gender, int contact, string course)
         {
-            this.regno = regno;
-            this.name = name;
-            this.dob = dob;
-            this.gender = gender;
-            this.contact = contact;
-            this.course = course;
+            this._regno = regno;
+            this._name = name;
+            this._dob = dob;
+            this._gender = gender;
+            this._contact = contact;
+            this._course = course;
+        }
+
+        public int regno
+        {
+            get { return _regno; }
+            set { _regno = value; }
+        }
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public DateTime dob
+        {
+            get { return _dob; }
+            set { _dob = value; }
+        }
+
+        public char gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public int contact
+        {
+            get { return _contact; }
+            set { _contact = value; }
+        }
+
+        public string course
+        {
+            get { return _course; }
+            set { _course = value; }
         }
     }
 }
